8aafb5b4384493bdaf889008b92e9d3ff20c3ebe
0x19231 <smigasuk@gmail.com>
2022-09-17 18:41:58 +0300

    dz dla Класи. Атрибути та методи класів.

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("hello to Igor Kozovyj")
+class dogger:
+    def __init__(self, name, age,whatdoesdogsay,wheredoglive):
+        self.name = name
+        self.whatdoesdogsay = whatdoesdogsay
+        self.wheredoglive = wheredoglive
+        self.age = age
+
+
+dog = dogger("Barsik","idk isnt my dog","Bark","idk not my dog")
+print("Name: {}\n"
+      "Age: {}\n"
+      "What does doggo say: {}\n"
+      "Where does doggo live: {}".format(dog.name,dog.age,dog.whatdoesdogsay,dog.wheredoglive))
+
+
